a089eed1be5f4479690a5a79cee3f05ac7fca748
Benzyme <97699370+benzyme16@users.noreply.github.com>
2022-08-18 14:44:34 +0200

    BubbleAI

---
+++ bubble beta/Config.lua
@@ -0,0 +1,15 @@
+--[[ This protection is pretty much just drag n' drop and it will have a strong effect against cheaters using client modifications to receive benefits ingame.
+
+This is the best free fivem ai/ac to take advantage against hackers, our advanced system and strong ban system its easy to keep your bans nice and orginized,
+Please do note that this script will not cause any false positives which means players complaining about their ban should not be trusted even if its an admin or not,
+This is an AI system made for drag n drop, this config is made for your server preference whether its low-end or high-end, this script does not take any performance
+and you can add your webhook logs here in this config.
+
+If you have any questions feel free to contact me. (Please do not spam me or ask obvious dumb questions, use your brain)
+]]
+
+Config = {}
+
+Config.Webhook = "WEBHOOK" -- Replace with your webhook for receiving logs.
+
+Config.BanLength = 31 -- ban length (IN DAYS, if you want permanent set to 0)
---
+++ bubble beta/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+description 'Made with love by Benzyme'
+author 'Benzyme#5979'
+version '1.0.0'
+
+server_scripts {
+    'files/server/main_sv.lua',
+    'files/server/cable.lua',
+    'Config.lua'
+}
+
+client_scripts {
+    'files/client/main_cl.lua'
+}
